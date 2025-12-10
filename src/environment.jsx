b459292c4e9222081c18df5ff0c16eef95a56fa8
MatiasPena <94708374+MatiasPena@users.noreply.github.com>
2025-12-10 17:49:49 -0300

    asd

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: `https://buquesinvoiceprod.brazilsouth.cloudapp.azure.com/api/`,
+    API_URL: `/api/`,
     PDF_NULL: `https://buquesinvoicestorage.blob.core.windows.net/invoices/Factura_sin_pdf_para_ingresos_sin_pdf.pdf`, 
 }
