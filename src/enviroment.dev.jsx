0484fe37519631ff95846b3ac53603fe8bba1159
MatiasPena <94708374+MatiasPena@users.noreply.github.com>
2025-12-10 15:52:43 -0300

    update de env

--- src/enviroment.dev.jsx
+++ src/enviroment.dev.jsx
@@ -1,3 +1,5 @@
 export const environment = {
-    API_URL: `http://localhost:5000/api/`
+    API_URL: `http://localhost:5000/api/`,
+
+    PDF_NULL: `https://buquesinvoicestorage.blob.core.windows.net/invoices/Factura_sin_pdf_para_ingresos_sin_pdf.pdf`, 
 }
--- src/environment.prod.jsx
+++ src/environment.prod.jsx
@@ -1,3 +1,4 @@
 export const environment = {
-    API_URL: `https://buques-invoice-ca3.icyplant-01d84587.eastus.azurecontainerapps.io/api/`
+    API_URL: `https://buquesinvoiceprod.brazilsouth.cloudapp.azure.com/api/`,
+    PDF_NULL: `https://buquesinvoicestorage.blob.core.windows.net/invoices/Factura_sin_pdf_para_ingresos_sin_pdf.pdf`, 
 }
